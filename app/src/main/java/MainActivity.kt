1e4dfe85fc1df6061e9de535f8adeab812f321a3
vBelyi <slava.belyi16@gmail.com>
2023-08-17 22:59:48 +0300

    made layout

---
+++ app/src/main/java/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kt_less10
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity: Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_layout)
+    }
+}
